2735c7e80039d6af81590c5c42e6a02aa897463b
Alex <alex.A4.25@yandex.ru>
2018-07-23 18:15:18 +0300

    Changed names of methods, removed ability to add synonyms if record's name stand at first position in dictionary's line

--- src/com/alexa4/synonyms/SynonymsExtractor.java
+++ src/com/alexa4/synonyms/SynonymsExtractor.java
@@ -15,42 +15,45 @@ import java.util.Scanner;
 public class SynonymsExtractor {
 
 	/**
-	 * Find a synonyms in dictionary with standard file way and add it to thesaurus
+	 * Find a synonyms in dictionary with standard file path and add it to thesaurus
 	 * Will add synonyms if word stand in any position into dictionary's line
 	 * @param thesaurus thesaurus with records
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 */
-	public static void findSynonymsFromWholeDictionary(Thesaurus thesaurus) throws FileNotFoundException, IOException{
+	public static void findSynonymsFromWholeDictionary(Thesaurus thesaurus) throws
+			FileNotFoundException, IOException{
 		File dictionaryFile = createDictFile();
-		readAndFindSynonyms(dictionaryFile, thesaurus);
+		extractSynonymsFromWholeLine(dictionaryFile, thesaurus);
 	}
 
+
 	/**
-	 * Find a synonyms in dictionary with input file way and add it to thesaurus
+	 * Find a synonyms in dictionary with input file path and add it to thesaurus
 	 * Will add synonyms if word stand in any position into dictionary's line
 	 * @param thesaurus thesaurus with records
-	 * @param fullFilePath full file way
+	 * @param fullDictionaryFilePath full file path for dictionary
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 */
-	public static void findSynonymsFromWholeDictionary(Thesaurus thesaurus, String fullFilePath) throws FileNotFoundException, IOException{
-		File dictionaryFile = createDictFile(fullFilePath);
-		readAndFindSynonyms(dictionaryFile, thesaurus);
+	public static void findSynonymsFromWholeDictionary(Thesaurus thesaurus, String fullDictionaryFilePath) throws
+			FileNotFoundException, IOException{
+		File dictionaryFile = createDictFile(fullDictionaryFilePath);
+		extractSynonymsFromWholeLine(dictionaryFile, thesaurus);
 	}
 
 
 	/**
 	 * 1) Reading every line from dictionary
 	 * 2) For each record from thesaurus trying to find synonyms in this line
-	 * 3.1) If they have found, and their count more than 1, then create new connection
+	 * 3) If they have found, and their count more than 1, then create new connection
 	 *      between all suitable records, if it have not created
-	 * 3.2) If record's name was stand at first position in dictionary's line, then add all synonyms to record
 	 * @param dictionaryFile to read
+	 * @param thesaurus link to thesaurus in memory
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 */
-	private static void readAndFindSynonyms(File dictionaryFile, Thesaurus thesaurus)
+	private static void extractSynonymsFromWholeLine(File dictionaryFile, Thesaurus thesaurus)
 			throws FileNotFoundException, IOException{
 
 		ArrayList<Record> records = thesaurus.getRecords();
@@ -69,10 +72,10 @@ public class SynonymsExtractor {
 			//Array of synonyms from dictionary without stemming
 			String[] fullWords = lineFromDict.split("[|]+");
 
-			findSynonymsToEachRecord(words, records, fullWords);
+			findSynonymsToEachRecordFromWholeLine(words, records, fullWords);
 
 			//Need to see progress of work
-			if (numberOfLine % 50000 == 0)
+			if (numberOfLine % 5000 == 0)
 				System.out.println(numberOfLine);
 		}
 		System.out.println(numberOfLine);
@@ -81,47 +84,25 @@ public class SynonymsExtractor {
 	/**
 	 * Finding synonyms from one line for each record
 	 * If there were found more than 1 record then they connect with each other
-	 * Also adds synonyms from dictionary if record's name stand at first position in dictionary's line
 	 * @param words array of words from one line from dictionary
 	 * @param records list of thesaurus records
 	 */
-	private static void findSynonymsToEachRecord(String[] words, ArrayList<Record> records, String[] fullWords){
+	private static void findSynonymsToEachRecordFromWholeLine(String[] words,
+	                                                          ArrayList<Record> records, String[] fullWords){
 		//Each suitable record will add to that list
 		ArrayList<Record> recordsToSynonyms = new ArrayList<Record>();
 
 		for (int i = 0; i < records.size(); i++){
-			//Add record to recordsToSynonyms if it contains into words[]
-			if (isContainsInSynonyms(words, records.get(i).getNameAfterStem())){
+			//Add record to recordsToSynonyms if its name contains into words[]
+			if (isContainsInDictLine(words, records.get(i).getNameAfterStem())){
 				recordsToSynonyms.add(records.get(i));
 			}
-
-			/**
-			 * If you don't want to add synonyms from dictionary, then comment conditional operator below
-			 */
-			//Add synonyms from fullWords[] to record if record.name stand at 0 position in words[]
-			if (words.length != 0)
-				if (records.get(i).getNameAfterStem().equals(words[0])){
-					addSynonymsIfAtFirstPlace(records.get(i), fullWords);
-				}
 		}
 
 		if (recordsToSynonyms.size() > 1){
 			connectRecords(recordsToSynonyms);
 		}
 
-	}
-
-	/**
-	 * If record.name was stand at first place in dictionary then add synonyms from this line to record
-	 * if they not contains in record's synonyms
-	 * @param record which name was stand at first place in dictionary
-	 * @param fullWords array of words without stemming
-	 */
-	private static void addSynonymsIfAtFirstPlace(Record record, String[] fullWords){
-		for (String word: fullWords){
-			if (!isSynonymContainsInRecord(word, record))
-				record.addSynonym(word);
-		}
 	}
 
 
@@ -131,7 +112,7 @@ public class SynonymsExtractor {
 	 * @param name of record
 	 * @return true if contains, false otherwise
 	 */
-	private static boolean isContainsInSynonyms(String[] words, String name){
+	private static boolean isContainsInDictLine(String[] words, String name){
 		for(String word: words){
 			if (name.equals(word))
 			return true;
@@ -176,6 +157,10 @@ public class SynonymsExtractor {
 
 		return false;
 	}
+
+
+
+
 
 	/**
 	 * Creating file with standard file path
